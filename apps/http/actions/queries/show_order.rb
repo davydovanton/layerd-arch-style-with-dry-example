a09b20f1bbc7f2a0fbfdea08b41b5cf98669caf8
Anton Davydov <antondavydov.o@gmail.com>
2022-08-22 06:44:29 +0300

    Add information section "how to use serelizators in transport layer"

--- apps/http/actions/queries/show_order.rb
+++ apps/http/actions/queries/show_order.rb
@@ -20,6 +20,18 @@ module HTTP
           case result
           in Success
             res.status  = 200
+
+            # if you want to use serelezation you can do something like this:
+            #
+            # 1. create a new pure function-class in `apps/http/serelizations/../serelization_name.rb`
+            # 2. add specific logic. IMPORTANT: this logic should be pure
+            # 3. inject serelization object in to action `include Import[serelizator: 'http.serelizations. ... .serelization_name']`
+            # 4. call it in action: `res.body = serelizator.call(data)`
+            # 5. update fitness functions
+            # 6. add specs for serelizator in `spec/apps/http/serelizators/.../serelizator_name_spec.rb`
+            # ...
+            # 7. PROFIT
+
             res.body    = result.value!.to_json
           in Failure[_, error_message]
             halt 422, error_message.to_json
